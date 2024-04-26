19dc9946e94527f8ea3076910804d833211f7269
japheth-alt163 <japhethbaraka4@gmail.com>
2024-04-26 11:27:56 +0000

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,7 @@
 '''A module for working with Pascal's triangle.
 '''
 
+
 def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.

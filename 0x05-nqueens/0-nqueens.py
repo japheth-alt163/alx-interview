be019bce954afc9ecc9dc4b5ac18849b9043b0fc
japheth-alt163 <japhethbaraka4@gmail.com>
2024-05-30 15:33:36 +0000

    done

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+N-queen problem solver
+Solves the N-queen problem for any N >= 4
+"""
+import sys
+
+
+def n_q(t_arr, arr, col, i, n):
+    """
+    Find all possible solutions for the N-queen problem and return them in a list.
+
+    @t_arr: temporary list to store the points of a possible solution
+    @arr: list to store all solutions
+    @col: list to save columns used for a queen
+    @i: the current row of the chessboard
+    @n: the number of queens
+    """
+    if i > n:
+        arr.append(t_arr[:])
+        return arr
+
+    for j in range(n + 1):
+        if i == 0 or ([i - 1, j - 1] not in t_arr and
+                      [i - 1, j + 1] not in t_arr and
+                      j not in col):
+            if i > 1:
+                dia = 0
+                for k in range(2, i + 1):
+                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
+                        dia = 1
+                        break
+                if dia:
+                    continue
+            t_arr.append([i, j])
+            col.append(j)
+            n_q(t_arr, arr, col, i + 1, n)
+            col.pop()
+            t_arr.pop()
+
+    return arr
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    n_q_arr = n_q([], [], [], 0, n - 1)
+    for solution in n_q_arr:
+        print(solution)
